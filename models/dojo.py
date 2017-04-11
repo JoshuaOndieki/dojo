40df232e2ca13b78232f31f424f9964371262d4c
JoshuaOndieki <joshuaondieki8@gmail.com>
2017-04-11 22:19:01 +0300

    Random assign office and livngspace

--- models/dojo.py
+++ models/dojo.py
@@ -2,6 +2,8 @@ from models.fellow import Fellow
 from models.staff import Staff
 from models.livingspace import LivingSpace
 from models.office import Office
+import random
+
 class Dojo():
     def __init__(self):
         self.offices={}
@@ -20,14 +22,14 @@ class Dojo():
             return('Room %s exists!'%(name))
         else:
             if room_type.lower()=='office'.lower():
-                room=Office(name)
-                self.offices[name]=room.members
-                self.all_rooms[name]=[room_type,room.members]
+                self.room=Office(name)
+                self.offices[name]=self.room.members
+                self.all_rooms[name]=[room_type,self.room.members]
                 return('An office called %s has been created successfully!'%(name))
             elif room_type.lower()=='livingspace'.lower():
-                room=LivingSpace(name)
-                self.livingspaces[name]=room.members
-                self.all_rooms[name]=[room_type,room.members]
+                self.room=LivingSpace(name)
+                self.livingspaces[name]=self.room.members
+                self.all_rooms[name]=[room_type,self.room.members]
                 return('A Livingspace called %s has been created successfully!'%(name))
 
     def add_person(self, firstname,surname,person_type,wants_accomodation='N'):
@@ -42,9 +44,25 @@ class Dojo():
                 fellow=Fellow(firstname,surname)
                 self.fellows.append(firstname +' '+ surname)
                 self.all_people.append(firstname +' '+ surname)
+                if self.offices:
+                    office=random.choice(list(self.offices))
+                    self.offices[office].append(firstname +' '+ surname)
+                    print('Fellow %s %s has been assigned office %s!'%(firstname,surname,office))
+                else:
+                    print('No office to assign!')
+                if wants_accomodation=='Y' and self.livingspaces:
+                    room=random.choice(list(self.livingspaces))
+                    self.livingspaces[room].append(firstname +' '+ surname)
+                    print('Fellow %s %s has been assigned livingspace %s!'%(firstname,surname,room))
                 return('Fellow %s %s has been added successfully!'%(firstname,surname))
             elif person_type.lower()=='staff':
                 staff=Staff(firstname,surname)
                 self.staff.append(firstname +' '+ surname)
                 self.all_people.append(firstname +' '+ surname)
+                if self.offices:
+                    office=random.choice(list(self.offices))
+                    self.offices[office].append(firstname +' '+ surname)
+                    print('Staff %s %s has been assigned office %s!'%(firstname,surname,office))
+                else:
+                    print('No office to assign!')
                 return('Staff %s %s has been added successfully!'%(firstname,surname))

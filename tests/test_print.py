b9cb2e0c0bc2d5229cae90d8a2d02cea17cc05a4
JoshuaOndieki <joshuaondieki8@gmail.com>
2017-04-12 12:13:06 +0300

    Unittest for print functions

--- tests/test_print.py
+++ tests/test_print.py
@@ -1,8 +1,11 @@
 import unittest
 from models.dojo import Dojo
+from pathlib import Path
+import os
 
 class TestPrint(unittest.TestCase):
     def setUp(self):
+        """ SetUp Dojo instance and populate test data"""
         self.instance=Dojo()
         offices=['Blue','Brown','Band']
         livingspaces=['Black','Bold','Blueberry']
@@ -17,3 +20,37 @@ class TestPrint(unittest.TestCase):
                 self.instance.add_person(person[0],person[1],person[2],person[3])
             else:
                 self.instance.add_person(person[0],person[1],person[2])
+        self.instance.print_allocations('test.txt')
+        self.instance.print_unallocations('test1.txt')
+
+    def tearDown(self):
+        """Delete all files used during testing"""
+        os.remove('test.txt')
+        os.remove('test1.txt')
+
+    def test_print_names_of_people_in_specific_room(self):
+        people_in_Blue=self.instance.all_rooms['Blue'][1]
+        query_with_room_name=self.instance.print_room('Blue')
+        self.assertEqual(query_with_room_name,people_in_Blue)
+
+    def test_print_allocations_without_file(self):
+        get_allocations={k: v[1] for k, v in self.instance.all_rooms.items() if v[1]}
+        print_allocations = self.instance.print_allocations()
+        self.assertEqual(print_allocations,get_allocations)
+
+    def test_print_allocations_with_file(self):
+        test_file = Path("test.txt")
+        self.assertTrue(if test_file.is_file())
+
+    def test_print_unallocations_without_file(self):
+        allocated_people = []
+        for key, value in self.instance.all_rooms.items():
+            for person in value[1]:
+                allocated_people.append(person)
+        unallocated_people = [p for p in self.instance.all_people if p is not in allocated_people]
+        print_unallocations = self.instance.print_unallocations()
+        self.assertEqual(print_unallocations,unallocated_people)
+
+    def test_print_unallocations_with_file(self):
+        test_file = Path("test1.txt")
+        self.assertTrue(if test_file.is_file())
